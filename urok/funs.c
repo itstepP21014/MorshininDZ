f01f08aa5b583e35f1bd30f5b49477cf02d34f1a
3A9C <deiviah@gmail.com>
2015-01-19 17:59:10 +0000

    urok

---
+++ urok/funs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void sayHello()
+{
+	printf("Привет!!\n");
+}
+
+void sayGoodbye()
+{
+	printf("Покеда\n");
+}
---
+++ urok/funs.h
@@ -0,0 +1,7 @@
+#ifndef FUNS_H
+#define FINS_H
+
+void sayHello();
+void sayGoodbye();
+
+#endif //FUSH_H
